14153c7dff55ff4c141b57c19efa2c51de395908
yurik <yurik.valder@gmail.com>
2021-07-05 19:02:56 -0400

    updating routing

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path'); 
 
 const port = process.env.PORT || 3007; 
 
+// app.get('/', function (req, res) {
+//     res.render('public', {});
+//   });
+
+app.use(express.static('public')); 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "Public", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Connected to port ${port}`);
 }); 
-
-app.use(express.static('public')); 
