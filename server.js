352bf86be289a222e7e05fa8f927ddc697260da4
Yurik Valderrama <yurik.valder@gmail.com>
2025-10-18 23:56:45 -0500

    Serve static files from project root

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3007; 
+const port = process.env.PORT || 3007;
 
-app.use(express.static('Public')); 
-
+// Serve static files from the project root
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
     console.log(`Connected to port ${port}`);
-}); 
+});
